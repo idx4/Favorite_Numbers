3b6d71945241ef224a7633be8d3776f85d2230eb
edwinwells <edwin.wallace@yandex.com>
2017-02-15 18:29:26 -0500

    HIDDEN METHOD TECHNIQUE:   This is the final form of the application/view re: utilizing the 'hidden view' method to store/transfer data throughout a program.  There is 1 app.rb, and there are 6 Views.  Operation works properly.

--- app.rb
+++ app.rb
@@ -1,17 +1,89 @@
-require "sinatra"
+require 'sinatra'
 
 
-get '/' do	
+
+get '/' do
+
 	erb :get_name
+
 end
 
-post '/post_name' do
+
+
+post '/name' do
+
 	name = params[:user_name]
-	erb :location, :locals => {:name => name}
+
+#	redirect '/age?user_name=' + name
+
+	erb :get_location, :locals => {:name => name}
+
+#	"Hello #{name} "
+
 end
+
+
 
 post '/location' do
-	location=params[:user_location]
+
+	location = params[:user_location]
+
 	name = params[:user_name]
-"#{name} your location is #{location}."
+
+	erb :get_age, :locals => {:name => name, :location => location}
+
+#	"#{name} your location is #{location}"
+
+end
+
+
+
+post '/age' do
+
+	age = params[:user_age]
+
+	location = params[:user_location]
+
+	name = params[:user_name]
+
+	erb :get_favnumbers, :locals => {:name => name, :location => location, :age => age}
+
+end
+
+
+
+post '/favorites' do
+
+	num1 = params[:num1]
+
+	num2 = params[:num2]
+
+	num3 = params[:num3]
+
+	age = params[:user_age]
+
+	location = params[:user_location]
+
+	name = params[:user_name]
+
+	sum_of_nums = num1.to_i + num2.to_i + num3.to_i
+
+	if sum_of_nums > age.to_i
+
+		age_compare = "Your favorite nums added together is greater than your age"
+
+	elsif sum_of_nums < age.to_i
+
+		age_compare = "Your favorite nums added together is less than your age"
+
+	else
+
+		age_compare = "Your favorite nums added together is equal to your age"
+
+	end
+
+#	"#{name} #{location} #{age} #{num1} #{num2} #{num3} #{sum_of_nums} #{age_compare}"
+
+	erb :showinfo, :locals => {:name => name, :location => location, :age => age, :num1 => num1, :num2 => num2, :num3 => num3, :sum_of_nums => sum_of_nums, :age_compare => age_compare}
+
 end
